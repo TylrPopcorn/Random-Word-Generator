a0d723ad01582d7f8a1cf499be131e952697a733
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-01-30 12:37:21 -0500

    working on

--- api/server.js
+++ api/server.js
@@ -7,12 +7,38 @@ require("dotenv").config();
  * npx eslint --init
  * npm i express
  * npm install dotenv
+ * npm i cors
  *
  */
 const PORT = process.env.PORT || 9000;
 const express = require("express");
-
+//
+const cors = require("cors");
+//
 const server = express();
 
 //GLOBAL MIDDLEWARE
+//-------------------
 server.use(express.json()); //Allow the use of json request body fields
+server.use(cors()); //enable cross-origin requests
+
+server.get("/", (req, res) => {
+  //base endpoint.
+  res.send(`
+        <h1> Welcome to Node! <h1>
+    `);
+});
+
+//Catch any endpoints that cannot be found:
+server.use("*", (req, res) => {
+  //catch all 404 errors middleware:
+  res.status(404).json({
+    message: `${req.method} ${req.baseUrl} not found`,
+  });
+});
+
+//ENDPOINTS
+//-----------
+server.listen(PORT, () => {
+  console.log(`\nServer is running on port http://localhost:${PORT}\n`);
+});
